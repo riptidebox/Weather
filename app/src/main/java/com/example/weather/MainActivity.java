f240d4fa5c847d5c9f85d3a8658729f4d4a02794
riptidebox <57669173+riptidebox@users.noreply.github.com>
2021-11-22 12:35:32 +0500

    Initial commit

---
+++ app/src/main/java/com/example/weather/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.weather;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private  EditText user_field;
+    private Button main_btn;
+    private TextView result_info;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        user_field = findViewById(R.id.user_field);
+        main_btn = findViewById(R.id.main_btn);
+        result_info = findViewById(R.id.result_info);
+
+        main_btn.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view) {
+                if (user_field.getText().toString().trim().equals(""))
+                    Toast.makeText(MainActivity.this, R.string.no_user_input, Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
